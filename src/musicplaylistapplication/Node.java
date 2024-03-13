f4b20c866330dc95a539f5a80b7b15c8f9025919
Yonas <116105802+yonas255@users.noreply.github.com>
2024-03-13 21:57:50 +0000

    creating a playlist application

---
+++ src/musicplaylistapplication/Node.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package musicplaylistapplication;
+
+/**
+ *
+ * @author yonas
+ */
+public class Node {
+   private Object element;
+   private Node next;
+
+    public Node(Object element, Node next) {
+        this.element = element;
+        this.next = next;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+   
+   
+}
+
+
